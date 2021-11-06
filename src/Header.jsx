f0a6fa9b49befa7cd1393567c45c1e31677ebca1
adhyaadam <adhyaadam@gmail.com>
2021-11-07 01:35:28 +0800

    Fix css for login

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,7 +12,7 @@ export default class Header extends React.Component {
     render() {
         return (<div className="header">
             <img src={this.props.lambang ? this.props.lambang : "/Borneo.png"} width="80" alt=""/>
-            <h1><Link style={{color:"black",textDecoration:"transparent"}} to="/"> {this.props.children} </Link></h1>	
+            <h1><Link style={{color:"black",textDecoration:"transparent",fontSize:"30px"}} to="/"> {this.props.children} </Link></h1>	
             <input onChange={ev=>{
                 this.setState({value:ev.target.value});
             }} id="query-search" onKeyPress={ev=>{
